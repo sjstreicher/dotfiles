5737e4afad341a925652b534f9640052e300e280
simon <streichersj@gmail.com>
2023-01-01 19:01:03 +0000

    null-ls config

---
+++ .config/nvim/after/plugin/null-ls.lua
@@ -0,0 +1,18 @@
+local null_ls = require("null-ls")
+
+null_ls.setup({
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.diagnostics.eslint,
+		-- null_ls.builtins.completion.spell,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.diagnostics.pylint.with({
+			prefer_local = function()
+				if vim.env.VIRTUAL_ENV then
+					return vim.env.VIRTUAL_ENV .. "/bin/pylint"
+				end
+			end,
+		}),
+	},
+})
--- .config/nvim/lua/volcanonix/packer.lua
+++ .config/nvim/lua/volcanonix/packer.lua
@@ -24,31 +24,35 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+  use({
+      'VonHeikemen/lsp-zero.nvim',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-buffer'},
+          {'hrsh7th/cmp-path'},
+          {'saadparwaiz1/cmp_luasnip'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'hrsh7th/cmp-nvim-lua'},
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},
+          {'rafamadriz/friendly-snippets'},
+      }
+  })
   use('mfussenegger/nvim-dap')
-  -- use('mfussenegger/nvim-dap-python')
+  use('mfussenegger/nvim-dap-python')
   use('rcarriga/nvim-dap-ui')
   use('theHamsta/nvim-dap-virtual-text')
   use('nvim-telescope/telescope-dap.nvim')
   -- use {'jayp0521/mason-nvim-dap.nvim', after = 'mason.nvim'}
+  use({
+      "jose-elias-alvarez/null-ls.nvim",
+      requires = { "nvim-lua/plenary.nvim" },
+  })
 end)
